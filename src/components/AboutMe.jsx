7bf9403612791829ea91b7664aaff767646bbdfa
unknown <efraine387@gmail.com>
2023-02-07 19:00:42 -0500

    added capstone and updated about

--- src/components/AboutMe.jsx
+++ src/components/AboutMe.jsx
@@ -7,8 +7,8 @@ const AboutMe=()=>{
         <div className='aboutMeContainer'>
           <h2>About Me</h2>
           <p>I'm an experienced Full-stack developer with a strong background in customer service and a proven ability to deliver high-quality, innovative web applications. 
-            Skilled in various programming languages including JavaScript, Python, and Java, as well as popular technologies such as React, Angular, Node.js, and Express. 
-            Strong understanding of database management, including MySQL, MongoDB, and Firebase. Adept at troubleshooting and resolving technical issues, and able to work effectively in 
+            Skilled in various programming languages including JavaScript, Python, and Java, as well as popular technologies such as React, Django, Angular, Node.js, and Express. 
+            Strong understanding of database management, including PostgreSQL, MongoDB, and Firebase. Adept at troubleshooting and resolving technical issues, and able to work effectively in 
             a team environment. A dedicated professional with a strong ability to understand and meet customer needs, resulting in the successful delivery of projects.</p>
           <div align="center">
             <code><img height="48" src="https://user-images.githubusercontent.com/25181517/117201156-9a724800-adec-11eb-9a9d-3cd0f67da4bc.png" alt="Java" title="Java" /></code>
--- src/components/Projects.jsx
+++ src/components/Projects.jsx
@@ -6,8 +6,10 @@ const Projects=()=>{
     <div className='projects'>
       <h2>Projects</h2>
       <div className='mainProjects'>
+        <a href='https://travelogue.herokuapp.com'>Travelogue</a>
+        <img className='TLimg' src='https://i.gyazo.com/a58e1fdb0a1a097b63abbab19b7741cd.jpg' width={800}/>
         <a href='https://fotobook-dark.netlify.app/'>Fotobook</a>
-        <img className='FBimg' src='' />
+        <img className='FBimg' src='https://i.gyazo.com/13e2920de1b35e6b31e236490f445bdc.png' width={800}/>
         <a href='https://HAPI-HOUR.netlify.app/'>HAPI HOUR</a>
         <img className='HHimg' src='https://camo.githubusercontent.com/81f8a71f750107cd04914867f2d42645934782171a222a26a287018d8708b386/68747470733a2f2f696d6775722e636f6d2f43784558506b442e6a7067' />
       </div>
