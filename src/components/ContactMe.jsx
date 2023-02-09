0ae548e7b21ba35478d275398f6cbd664a8cd0ae
unknown <efraine387@gmail.com>
2023-02-09 14:09:25 -0500

    added github

--- src/components/ContactMe.jsx
+++ src/components/ContactMe.jsx
@@ -10,9 +10,18 @@ const ContactMe=()=>{
         <a href="https://linkedin.com/in/efrainencarnacion" target="blank">
           <img align="center" 
                src="https://raw.githubusercontent.com/rahuldkjain/github-profile-readme-generator/master/src/images/icons/Social/linked-in-alt.svg" 
-               alt="efrainencarnacion" 
+               alt="LinkedIn" 
                height="30" 
                width="40" />
+        </a>
+      </p>
+      <p>
+        <a href="https://github.com/efrainenc" target="blank">
+          <img align="center" 
+                src="https://banner2.cleanpng.com/20180824/jtl/kisspng-computer-icons-logo-portable-network-graphics-clip-icons-for-free-iconza-circle-social-5b7fe46b0bac53.1999041115351082030478.jpg" 
+                alt="GitHub" 
+                height="30" 
+                width="40" />
         </a>
       </p>
     </div>
