b6eed00623da532d58163f1f6ae3348f2ddf299a
unknown <efraine387@gmail.com>
2023-02-12 12:38:37 -0500

    fixing Project titles

--- src/components/Projects.jsx
+++ src/components/Projects.jsx
@@ -8,31 +8,38 @@ const Projects=()=>{
       <div className='mainProjects'>
         <div className='projectCard'>
           <img className='TLimg' src='https://i.gyazo.com/a58e1fdb0a1a097b63abbab19b7741cd.jpg' />
-          <a href='https://travelogue.herokuapp.com'>Travelogue</a>
+          <h2>Travelogue</h2>
+          <a href='https://travelogue.herokuapp.com'>Link to site</a>
           <p>
-            Django, Python, PostgreSQL, OAuth2.0, Bootstrap
             Full-stack travel app for those who love to travel that helps you plan and log your trips
             Deployed a full stack application to Heroku utilizing Django + Bootstrap with full CRUD within a 1 week sprint.
             Utilized Django AllAuth to implement OAuth2.0 Google Authentication and allow for the future implementation of Google API's.
             Exhibited an advanced understanding of Authentication, Python, and PostgreSQL.
             Developed a robust and scalable backend system using Python, Django, and PostgreSQL.
           </p>
+          <p>
+          Django, Python, PostgreSQL, OAuth2.0, Bootstrap
+          </p>
         </div>
         <div className='projectCard'>
           <img className='FBimg' src='https://i.gyazo.com/13e2920de1b35e6b31e236490f445bdc.png' />
-          <a href='https://fotobook-dark.netlify.app/'>Fotobook</a>
+          <h2>Fotobook</h2>
+          <a href='https://fotobook-dark.netlify.app/'>Link to site</a>
           <p>
-            MERN Stack, JS, HTML, CSS
             Full-stack app collaboration that recreates the Instagram user experience 
             Collaborated to utilize a MERN stack (MongoDB, Express, React.js, Node.js) to build a full stack application in 3 days. 
             Implemented JWT Authentication to allow user logins.
             Completed full CRUD operations hitting all RESTful API calls.
             Created an application with a mobile-first approach and styled using Styled-Components and CSS3 with flexbox layouts.
           </p>
+          <p>
+          MERN Stack, JS, HTML, CSS
+          </p>
         </div>
         <div className='projectCard'>
           <img className='HHimg' src='https://camo.githubusercontent.com/81f8a71f750107cd04914867f2d42645934782171a222a26a287018d8708b386/68747470733a2f2f696d6775722e636f6d2f43784558506b442e6a7067' />
-          <a href='https://HAPI-HOUR.netlify.app/'>HAPI HOUR</a>
+          <h2>HAPI HOUIR</h2>
+          <a href='https://HAPI-HOUR.netlify.app/'>Link to site</a>
           <p>
             React, JS, API Integration
             React app that utilizes API calls to provide the user with any cocktail of various categories
