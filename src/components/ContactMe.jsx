07015001fb2ceeb99db52347dfc08280cc0d4939
unknown <efraine387@gmail.com>
2023-03-28 21:05:51 -0400

    bigger resume and new contact me form

--- src/components/ContactMe.jsx
+++ src/components/ContactMe.jsx
@@ -5,7 +5,27 @@ const ContactMe=()=>{
   return (
     <section id='contactMe'>
       <h1>Contact Me</h1>
-      <h2>Email: evanencarnacion3@gmail.com</h2>
+      <form action="#" method="POST">
+        <div>
+          <label for="name">Name:</label>
+          <input type="text" id="name" name="name" required/>
+        </div>
+        <div>
+          <label for="email">Email:</label>
+          <input type="email" id="email" name="email" required/>
+        </div>
+        <div>
+          <label for="subject">Subject:</label>
+          <input type="text" id="subject" name="email_subject" required/>
+        </div>
+        <div>
+          <label for="message">Message:</label>
+          <textarea id="message" name="user_message" required></textarea>
+        </div>
+        <div>
+          <input type="submit" value="Send"/>
+        </div>
+      </form>
     </section>
   )
 }
