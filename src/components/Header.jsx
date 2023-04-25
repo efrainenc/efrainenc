f1debfce66886fcd6d1f09239c2da27363e27764
evanenc <efraine387@gmail.com>
2023-04-24 20:28:10 -0600

    changing resume in header

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -7,7 +7,7 @@ const Header=()=>{
       <a href="#aboutMe">01. About</a>
       <a href="#projects">02. Projects</a>
       <a href="#contact">03. Contact</a>
-      <a id='resume' href='https://docs.google.com/document/d/10W3QkQ4qblCWNu1jrTB4JLTZrNNZWe4EXcJhQmd-hbw/edit?usp=sharing'>My Resume</a>
+      <a id='resume' href='https://docs.google.com/document/d/10W3QkQ4qblCWNu1jrTB4JLTZrNNZWe4EXcJhQmd-hbw/edit?usp=sharing'>Resume</a>
     </section>
   )
 }
