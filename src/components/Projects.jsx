c9920e01be8e5e2665d92f4f7042c1d1b01f9764
evanenc <efraine387@gmail.com>
2023-04-29 11:08:47 -0600

    adding animations to the rest of the components and removing unused emailjs

--- src/components/Projects.jsx
+++ src/components/Projects.jsx
@@ -1,84 +1,89 @@
 import React from 'react'
+import { AnimationOnScroll } from 'react-animation-on-scroll';
+import "animate.css/animate.min.css";
 import '../css/Projects.css'
 
 const Projects=()=>{
   return(
-    <div id='projects'>
-      <br></br>
-      <br></br>
-      <br></br>
-      <div>
-        <h2 className='sectionTitle'><span>03. </span>Projects</h2>
+    <AnimationOnScroll animateIn="animate__fadeInUp">
+        <div id='projects'>
         <br></br>
+        <br></br>
+        <br></br>
+        <div>
+          <h2 className='sectionTitle'><span>03. </span>Projects</h2>
+          <br></br>
+        </div>
+        <div className='mainProjects'>
+          <div className='projectCard'>
+            <img className='TLimg' src='https://i.gyazo.com/a58e1fdb0a1a097b63abbab19b7741cd.jpg' />
+            <div className='projectInfo'>
+              <h2>Travelogue</h2>
+              <p>
+                A Full stack travel app that helps you plan, track and log your trips w/OAuth2.0 for Google Authentication
+              </p>            
+              <p className='projTech'>
+                Tech: Django, Python, OAuth2.0, PostgreSQL, Bootstrap
+              </p>
+              <div className='projLinks'>
+                <a target="_blank" href='https://travelogue.herokuapp.com'>[Demo]</a>
+                <a target="_blank" href='https://github.com/evanenc/travelogue'>[GitHub]</a>
+              </div>
+            </div>
+          </div>
+          <div className='projectCard'>
+            <img className='FBimg' src='https://i.gyazo.com/13e2920de1b35e6b31e236490f445bdc.png' />
+            <div className='projectInfo'>
+              <h2>FotoBook</h2>
+              <p>
+                A Full stack app collaboration that recreates the Instagram user experience. Utilizing MERN stack and JWT Authentication. 
+                Complete with full CRUD operations hitting all RESTful API calls.
+              </p>
+              <p className='projTech'>
+                Tech: MERN Stack, JavaScript, HTML, CSS
+              </p>
+              <div className='projLinks'>
+                <a target="_blank" href='https://fotobook-dark.netlify.app/'>[Demo]</a>
+                <a target="_blank" href='https://github.com/evanenc/project-3-fe/tree/efrain_dev'>[GitHub]</a>
+              </div>
+            </div>
+          </div>
+          <div className='projectCard'>
+            <img className='HHimg' src='https://camo.githubusercontent.com/81f8a71f750107cd04914867f2d42645934782171a222a26a287018d8708b386/68747470733a2f2f696d6775722e636f6d2f43784558506b442e6a7067' />
+            <div className='projectInfo'>
+              <h2>HAPI HOUR</h2>
+              <p>
+                A React app collaboration that utilizes API calls to provide the user with any cocktail of various categories.
+              </p>
+              <p className='projTech'>
+                Tech: React, JavaScript, HTML, CSS
+              </p>
+              <div className='projLinks'>
+                <a target="_blank" href='https://HAPI-HOUR.netlify.app/'>[Demo]</a>
+                <a target="_blank" href='https://github.com/cbowman422/hapi-hour'>[Repo]</a>
+              </div>
+            </div>
+          </div>
+          <div className='projectCard'>
+          <img className='SSimg' src='https://camo.githubusercontent.com/19afd36991f68237a6c4bde3b927b9fa6298d1bb5e920e3dd444fadc7b15753b/68747470733a2f2f692e6779617a6f2e636f6d2f63386333393038613036303031383938663263626362316535623338656431332e6a7067' />
+            <div className='projectInfo'>
+              <h2>Simon Says</h2>
+              <p>
+                A classic Windows themed Simon Says game! Created with JS, HTML, and CSS, This was the very first game that I created to start my journey as a software developer! 
+              </p>
+              <p className='projTech'>
+                Tech: JavaScript, HTML, CSS
+              </p>
+              <div className='projLinks'>
+                <a target="_blank" href='https://evanenc.github.io/Project-1-Game/'>[Demo]</a>
+                <a target="_blank" href='https://github.com/evanenc/Project-1-Game'>[GitHub]</a>
+              </div>
+            </div>
+          </div>
+        </div>
       </div>
-      <div className='mainProjects'>
-        <div className='projectCard'>
-          <img className='TLimg' src='https://i.gyazo.com/a58e1fdb0a1a097b63abbab19b7741cd.jpg' />
-          <div className='projectInfo'>
-            <h2>Travelogue</h2>
-            <p>
-              A Full stack travel app that helps you plan, track and log your trips w/OAuth2.0 for Google Authentication
-            </p>            
-            <p className='projTech'>
-              Tech: Django, Python, OAuth2.0, PostgreSQL, Bootstrap
-            </p>
-            <div className='projLinks'>
-              <a target="_blank" href='https://travelogue.herokuapp.com'>[Demo]</a>
-              <a target="_blank" href='https://github.com/evanenc/travelogue'>[GitHub]</a>
-            </div>
-          </div>
-        </div>
-        <div className='projectCard'>
-          <img className='FBimg' src='https://i.gyazo.com/13e2920de1b35e6b31e236490f445bdc.png' />
-          <div className='projectInfo'>
-            <h2>FotoBook</h2>
-            <p>
-              A Full stack app collaboration that recreates the Instagram user experience. Utilizing MERN stack and JWT Authentication. 
-              Complete with full CRUD operations hitting all RESTful API calls.
-            </p>
-            <p className='projTech'>
-              Tech: MERN Stack, JavaScript, HTML, CSS
-            </p>
-            <div className='projLinks'>
-              <a target="_blank" href='https://fotobook-dark.netlify.app/'>[Demo]</a>
-              <a target="_blank" href='https://github.com/evanenc/project-3-fe/tree/efrain_dev'>[GitHub]</a>
-            </div>
-          </div>
-        </div>
-        <div className='projectCard'>
-          <img className='HHimg' src='https://camo.githubusercontent.com/81f8a71f750107cd04914867f2d42645934782171a222a26a287018d8708b386/68747470733a2f2f696d6775722e636f6d2f43784558506b442e6a7067' />
-          <div className='projectInfo'>
-            <h2>HAPI HOUR</h2>
-            <p>
-              A React app collaboration that utilizes API calls to provide the user with any cocktail of various categories.
-            </p>
-            <p className='projTech'>
-              Tech: React, JavaScript, HTML, CSS
-            </p>
-            <div className='projLinks'>
-              <a target="_blank" href='https://HAPI-HOUR.netlify.app/'>[Demo]</a>
-              <a target="_blank" href='https://github.com/cbowman422/hapi-hour'>[Repo]</a>
-            </div>
-          </div>
-        </div>
-        <div className='projectCard'>
-        <img className='SSimg' src='https://camo.githubusercontent.com/19afd36991f68237a6c4bde3b927b9fa6298d1bb5e920e3dd444fadc7b15753b/68747470733a2f2f692e6779617a6f2e636f6d2f63386333393038613036303031383938663263626362316535623338656431332e6a7067' />
-          <div className='projectInfo'>
-            <h2>Simon Says</h2>
-            <p>
-              A classic Windows themed Simon Says game! Created with JS, HTML, and CSS, This was the very first game that I created to start my journey as a software developer! 
-            </p>
-            <p className='projTech'>
-              Tech: JavaScript, HTML, CSS
-            </p>
-            <div className='projLinks'>
-              <a target="_blank" href='https://evanenc.github.io/Project-1-Game/'>[Demo]</a>
-              <a target="_blank" href='https://github.com/evanenc/Project-1-Game'>[GitHub]</a>
-            </div>
-          </div>
-        </div>
-      </div>
-    </div>
+    </AnimationOnScroll>
+    
   )
 }
 
--- src/components/Skills.jsx
+++ src/components/Skills.jsx
@@ -1,62 +1,67 @@
 import React from 'react'
+import { AnimationOnScroll } from 'react-animation-on-scroll';
+import "animate.css/animate.min.css";
 import '../css/Skills.css'
 
 export const Skills = () => {
   return (
-    <section id='skills'>
-      <div>
-        <h2><span>02. </span>Skills</h2>
-      </div>
-      <div className='skillsContainer'>
-        <div className='skillCategory'>
-          <h2>Languages</h2>
-          <br></br>
-          <div className='skillList'>
-            <img className='Python' src='https://img.shields.io/badge/python-3670A0?style=for-the-badge&logo=python&logoColor=ffdd54'/>
-            <img className='TypeScript' src='https://img.shields.io/badge/typescript-%23007ACC.svg?style=for-the-badge&logo=typescript&logoColor=white'/>
-            <img className='JavaScript' src='https://img.shields.io/badge/javascript-%23323330.svg?style=for-the-badge&logo=javascript&logoColor=%23F7DF1E'/>
-            <img className='C' src='https://img.shields.io/badge/c-%2300599C.svg?style=for-the-badge&logo=c&logoColor=white'/>
-            <img className='Java' src='https://img.shields.io/badge/java-%23ED8B00.svg?style=for-the-badge&logo=java&logoColor=white'/>
-            <img className='CSS3' src='https://img.shields.io/badge/css3-%231572B6.svg?style=for-the-badge&logo=css3&logoColor=white'/>
-            <img className='HTML5' src='https://img.shields.io/badge/html5-%23E34F26.svg?style=for-the-badge&logo=html5&logoColor=white'/>
-            <img className='C++' src='https://img.shields.io/badge/c++-%2300599C.svg?style=for-the-badge&logo=c%2B%2B&logoColor=white/'/>
+    <AnimationOnScroll animateIn="animate__fadeInUp">
+        <section id='skills'>
+        <div>
+          <h2><span>02. </span>Skills</h2>
+        </div>
+        <div className='skillsContainer'>
+          <div className='skillCategory'>
+            <h2>Languages</h2>
+            <br></br>
+            <div className='skillList'>
+              <img className='Python' src='https://img.shields.io/badge/python-3670A0?style=for-the-badge&logo=python&logoColor=ffdd54'/>
+              <img className='TypeScript' src='https://img.shields.io/badge/typescript-%23007ACC.svg?style=for-the-badge&logo=typescript&logoColor=white'/>
+              <img className='JavaScript' src='https://img.shields.io/badge/javascript-%23323330.svg?style=for-the-badge&logo=javascript&logoColor=%23F7DF1E'/>
+              <img className='C' src='https://img.shields.io/badge/c-%2300599C.svg?style=for-the-badge&logo=c&logoColor=white'/>
+              <img className='Java' src='https://img.shields.io/badge/java-%23ED8B00.svg?style=for-the-badge&logo=java&logoColor=white'/>
+              <img className='CSS3' src='https://img.shields.io/badge/css3-%231572B6.svg?style=for-the-badge&logo=css3&logoColor=white'/>
+              <img className='HTML5' src='https://img.shields.io/badge/html5-%23E34F26.svg?style=for-the-badge&logo=html5&logoColor=white'/>
+              <img className='C++' src='https://img.shields.io/badge/c++-%2300599C.svg?style=for-the-badge&logo=c%2B%2B&logoColor=white/'/>
+            </div>
+          </div>
+          <div className='skillCategory'>
+            <h2>Databases</h2>
+            <br></br>
+            <div className='skillList'>
+              <img className='MongoDB' src='https://img.shields.io/badge/MongoDB-%234ea94b.svg?style=for-the-badge&logo=mongodb&logoColor=white'/>
+              <img className='Postgres' src='https://img.shields.io/badge/postgres-%23316192.svg?style=for-the-badge&logo=postgresql&logoColor=white'/>
+              <img className='MySQL' src='https://img.shields.io/badge/mysql-%2300f.svg?style=for-the-badge&logo=mysql&logoColor=white'/>
+              <img className='Firebase' src='https://img.shields.io/badge/Firebase-039BE5?style=for-the-badge&logo=Firebase&logoColor=white'/>
+            </div>
+          </div>
+          <div className='skillCategory'>
+            <h2>Frameworks, Platforms and Libraries</h2>
+            <br></br>
+            <div className='skillList'>
+              <img className='React' src='https://img.shields.io/badge/react-%2320232a.svg?style=for-the-badge&logo=react&logoColor=%2361DAFB'/>
+              <img className='ExpressJS' src='https://img.shields.io/badge/express.js-%23404d59.svg?style=for-the-badge&logo=express&logoColor=%2361DAFB'/>
+              <img className='Django' src='https://img.shields.io/badge/django-%23092E20.svg?style=for-the-badge&logo=django&logoColor=white'/>
+              <img className='Angular' src='https://img.shields.io/badge/angular-%23DD0031.svg?style=for-the-badge&logo=angular&logoColor=white'/>
+              <img className='NodeJS' src='https://img.shields.io/badge/node.js-6DA55F?style=for-the-badge&logo=node.js&logoColor=white'/>
+              <img className='JWT' src='https://img.shields.io/badge/JWT-black?style=for-the-badge&logo=JSON%20web%20tokens'/>
+              <img className='SASS' src='https://img.shields.io/badge/SASS-hotpink.svg?style=for-the-badge&logo=SASS&logoColor=white'/>
+              <img className='Bootstrap' src='https://img.shields.io/badge/bootstrap-%23563D7C.svg?style=for-the-badge&logo=bootstrap&logoColor=white'/>
+              <img className='jQuery' src='https://img.shields.io/badge/jquery-%230769AD.svg?style=for-the-badge&logo=jquery&logoColor=white'/>
+              <img className='NPM' src='https://img.shields.io/badge/NPM-%23000000.svg?style=for-the-badge&logo=npm&logoColor=white'/>
+              <img className='Anaconda' src='https://img.shields.io/badge/Anaconda-%2344A833.svg?style=for-the-badge&logo=anaconda&logoColor=white'/>
+              <img className='Flask' src='https://img.shields.io/badge/flask-%23000.svg?style=for-the-badge&logo=flask&logoColor=white'/>
+              <img className='Svelte' src='https://img.shields.io/badge/svelte-%23f1413d.svg?style=for-the-badge&logo=svelte&logoColor=white'/>
+              <img className='Pytorch' src='https://img.shields.io/badge/PyTorch-%23EE4C2C.svg?style=for-the-badge&logo=PyTorch&logoColor=white'/>
+              <img className='Pandas' src='https://img.shields.io/badge/pandas-%23150458.svg?style=for-the-badge&logo=pandas&logoColor=white'/>
+              <img className='NumPy' src='https://img.shields.io/badge/numpy-%23013243.svg?style=for-the-badge&logo=numpy&logoColor=white'/>
+              <img className='Postman' src='https://img.shields.io/badge/Postman-FF6C37?style=for-the-badge&logo=postman&logoColor=white'/>
+            </div>
           </div>
         </div>
-        <div className='skillCategory'>
-          <h2>Databases</h2>
-          <br></br>
-          <div className='skillList'>
-            <img className='MongoDB' src='https://img.shields.io/badge/MongoDB-%234ea94b.svg?style=for-the-badge&logo=mongodb&logoColor=white'/>
-            <img className='Postgres' src='https://img.shields.io/badge/postgres-%23316192.svg?style=for-the-badge&logo=postgresql&logoColor=white'/>
-            <img className='MySQL' src='https://img.shields.io/badge/mysql-%2300f.svg?style=for-the-badge&logo=mysql&logoColor=white'/>
-            <img className='Firebase' src='https://img.shields.io/badge/Firebase-039BE5?style=for-the-badge&logo=Firebase&logoColor=white'/>
-          </div>
-        </div>
-        <div className='skillCategory'>
-          <h2>Frameworks, Platforms and Libraries</h2>
-          <br></br>
-          <div className='skillList'>
-            <img className='React' src='https://img.shields.io/badge/react-%2320232a.svg?style=for-the-badge&logo=react&logoColor=%2361DAFB'/>
-            <img className='ExpressJS' src='https://img.shields.io/badge/express.js-%23404d59.svg?style=for-the-badge&logo=express&logoColor=%2361DAFB'/>
-            <img className='Django' src='https://img.shields.io/badge/django-%23092E20.svg?style=for-the-badge&logo=django&logoColor=white'/>
-            <img className='Angular' src='https://img.shields.io/badge/angular-%23DD0031.svg?style=for-the-badge&logo=angular&logoColor=white'/>
-            <img className='NodeJS' src='https://img.shields.io/badge/node.js-6DA55F?style=for-the-badge&logo=node.js&logoColor=white'/>
-            <img className='JWT' src='https://img.shields.io/badge/JWT-black?style=for-the-badge&logo=JSON%20web%20tokens'/>
-            <img className='SASS' src='https://img.shields.io/badge/SASS-hotpink.svg?style=for-the-badge&logo=SASS&logoColor=white'/>
-            <img className='Bootstrap' src='https://img.shields.io/badge/bootstrap-%23563D7C.svg?style=for-the-badge&logo=bootstrap&logoColor=white'/>
-            <img className='jQuery' src='https://img.shields.io/badge/jquery-%230769AD.svg?style=for-the-badge&logo=jquery&logoColor=white'/>
-            <img className='NPM' src='https://img.shields.io/badge/NPM-%23000000.svg?style=for-the-badge&logo=npm&logoColor=white'/>
-            <img className='Anaconda' src='https://img.shields.io/badge/Anaconda-%2344A833.svg?style=for-the-badge&logo=anaconda&logoColor=white'/>
-            <img className='Flask' src='https://img.shields.io/badge/flask-%23000.svg?style=for-the-badge&logo=flask&logoColor=white'/>
-            <img className='Svelte' src='https://img.shields.io/badge/svelte-%23f1413d.svg?style=for-the-badge&logo=svelte&logoColor=white'/>
-            <img className='Pytorch' src='https://img.shields.io/badge/PyTorch-%23EE4C2C.svg?style=for-the-badge&logo=PyTorch&logoColor=white'/>
-            <img className='Pandas' src='https://img.shields.io/badge/pandas-%23150458.svg?style=for-the-badge&logo=pandas&logoColor=white'/>
-            <img className='NumPy' src='https://img.shields.io/badge/numpy-%23013243.svg?style=for-the-badge&logo=numpy&logoColor=white'/>
-            <img className='Postman' src='https://img.shields.io/badge/Postman-FF6C37?style=for-the-badge&logo=postman&logoColor=white'/>
-          </div>
-        </div>
-      </div>
-    </section>
+      </section>
+    </AnimationOnScroll>
+    
   )
 }
 
