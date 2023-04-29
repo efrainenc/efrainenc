6db0368241a66b2de1727cfab006f2ee521dbcb2
evanenc <efraine387@gmail.com>
2023-04-28 23:01:49 -0600

    fixing contact header link

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -7,7 +7,7 @@ const Header=()=>{
       <a href="#aboutMe"><span>01. </span>About</a>
       <a href="#skills"><span>02. </span>Skills</a>
       <a href="#projects"><span>03. </span>Projects</a>
-      <a href="#contact"><span>04. </span>Contact</a>
+      <a href="#contactMe"><span>04. </span>Contact</a>
       <a id='resume' href='https://docs.google.com/document/d/10W3QkQ4qblCWNu1jrTB4JLTZrNNZWe4EXcJhQmd-hbw/edit?usp=sharing'>Resume</a>
     </section>
   )
